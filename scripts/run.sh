4652b86af9108ed165df1c909049ca99c302a8a2
jorge <cinfacol@gmail.com>
2022-06-04 20:40:52 -0500

    actualizacion run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,7 @@ set -e
 
 python manage.py wait_for_db
 python manage.py collectstatic --noinput --clear
+python manage.py makemigrations cart inventario perfiles products store
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
